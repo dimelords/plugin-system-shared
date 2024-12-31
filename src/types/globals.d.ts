3d558a052238a80337512a7b03b9707cd8ef6888
Fredrik Gustafsson <fredrik@teamleader.se>
2024-12-31 18:28:26 +0100

    Initial commit

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -2,6 +2,11 @@ import type React from 'react';
 import type ReactDOM from 'react-dom';
 
 declare global {
+  namespace JSX {
+    interface Element extends React.ReactElement<any, any> { }
+    interface IntrinsicElements extends React.JSX.IntrinsicElements { }
+    interface ElementChildrenAttribute { children: {}; }
+  }
   interface Window {
     React: typeof React;
     ReactDOM: typeof ReactDOM;
